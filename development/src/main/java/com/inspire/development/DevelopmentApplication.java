b6c8f93cf65586edd0fe4b9c13bbc42c66004774
Lukas <lukas.gaebler@gmail.com>
2020-01-14 17:12:22 +0100

    Added Spring (Backend) and Angular (Frontend)

---
+++ development/src/main/java/com/inspire/development/DevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.inspire.development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentApplication.class, args);
+	}
+
+}
