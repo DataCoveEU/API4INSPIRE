ffcb7a89c42bf0ea07319eb77c445fb00d1f2802
Lukas <lukas.gaebler@gmail.com>
2020-01-17 23:23:25 +0100

    Started with the Dashboard

--- development/frontend/src/app/app-routing.module.ts
+++ development/frontend/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ErrorComponent } from './error/error.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '', component: LandingPageComponent },
   { path: 'error', component: ErrorComponent },
   { path: '**', redirectTo: '/error', pathMatch: 'full' }
