05155bda016e0f8b180fafecb7b4970171c7e249
TobiasPressler <pressler.tobias@gmail.com>
2020-01-20 14:02:39 +0100

    added api, with geometry

---
+++ development/src/main/java/com/inspire/development/collections/Link.java
@@ -0,0 +1,32 @@
+package com.inspire.development.collections;
+
+public class Link {
+    String href;
+    String rel;
+    String type;
+    String title;
+
+
+    public Link(String href, String rel, String type, String title){
+        this.href = href;
+        this.rel = rel;
+        this.type = type;
+        this.title = title;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getRel() {
+        return rel;
+    }
+}
