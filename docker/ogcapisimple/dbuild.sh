bd1866ef3a6d52f00f668f6d5a89341b29d64572
kgaebler <klaus.gaebler@austrocontrol.at>
2020-02-13 14:55:29 +0100

    unecessary path removed

--- docker/ogcapisimple/dbuild.sh
+++ docker/ogcapisimple/dbuild.sh
@@ -1,5 +1,4 @@
 # !/usr/bin/sh
 clear
 echo "Docker Build Image of OGC API Simple based on Tomcat"
-cd ~/Documents/Computer/Docker/tomcat/
 docker build -t tomcat:ogcapisimple .
--- docker/ogcapisimple/dconnect.sh
+++ docker/ogcapisimple/dconnect.sh
@@ -1,4 +1,3 @@
 # !/usr/bin/sh
 echo "Docker connect to bash of OGC API Simple based on Tomcat Container"
-cd ~/Documents/Computer/Docker/tomcat
 docker exec -it ogcapisimple-tomcat /bin/bash
