bfa21737770eb780297458d75d85f7f4d14045cc
Lukas <lukas.gaebler@gmail.com>
2020-04-06 15:26:45 +0200

    Added ogc api simple directory

--- docker/ogcapisimple/dconnect.sh
+++
@@ -1,3 +0,0 @@
-# !/usr/bin/sh
-echo "Docker connect to bash of OGC API Simple based on Tomcat Container"
-docker exec -it ogcapisimple-tomcat /bin/bash
--- docker/postgis/dconnect.sh
+++
@@ -1,3 +0,0 @@
-# !/usr/bin/sh
-echo "Docker connect to bash of PostGIS container"
-docker exec -it acgsdi-postgis /bin/bash
