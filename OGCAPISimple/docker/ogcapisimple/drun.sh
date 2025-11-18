f7d87299499b4d92d76748ecb7fdf5aac46c21e2
Hylke van der Schaaf <hylke.vds@gmail.com>
2025-11-18 16:03:01 +0100

    Removed obsolete OGCAPISimple

--- OGCAPISimple/docker/ogcapisimple/drun.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-echo "Docker Run --> OGC API Simple based on Tomcat"
-docker container rm -f ogcapisimple-tomcat
-#docker run -p 8643:8443 -it --name ogcapisimple-tomcat tomcat:ogcapisimple
-docker run -p 8443:8443 -d --name ogcapisimple-tomcat tomcat:ogcapisimple
